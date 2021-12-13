07efed96d56dc280c341d3f6deee0166181384c0
ecknight <ellyknight@gmail.com>
2021-12-12 22:32:30 -0700

    Write output

--- 08_BRT.R
+++ 08_BRT.R
@@ -7,65 +7,33 @@ library(lubridate)
 library(dismo)
 
 #1. Wrangle----
-dat <- read.csv("SurveyDataWithOffsets.csv")  %>% 
+dat <- read.csv("SurveyDataWithCovs.csv") %>% 
   mutate(ID=paste0(survey,"-",station,"-",year),
          boom=ifelse(detection==2, 1, 0),
          call=ifelse(detection>0, 1, 0),
          DateTime = ymd_hms(DateTime),
-         doy = yday(DateTime)) %>% 
-  arrange(ID, DateTime) %>% 
-  group_by(ID) %>% 
-  mutate(n=row_number()) %>% 
-  ungroup() %>% 
-  dplyr::filter(doy < 225)
+         doy = yday(DateTime))
 
-#2. Add covariates to data----
-cov <- read.csv("SurveyLocationsWithCovs.csv") %>% 
-  mutate(X=round(X),
-         Y=round(Y)) %>% 
-  unique()
-
-dat.cov <- dat %>% 
-  mutate(station = gsub(pattern="HORSE-0", replacement="HORSE-", x=station),
-         station = gsub(pattern="LAKE-0", replacement="LAKE-", x=station),
-         station = gsub(pattern="OIL-0", replacement="OIL-", x=station),
-         station = gsub(pattern="RRP-0", replacement="RRP-", x=station),
-         station = gsub(pattern="CLOUDY-0", replacement="CLOUDY-", x=station),
-         station = gsub(pattern="WLNP-006-001", replacement="WLNP-6-1", x=station),
-         station = gsub(pattern="WLNP-007-001", replacement="WLNP-7-1", x=station),
-         station = gsub(pattern="CONI-00", replacement="CONI-", x=station),
-         station = gsub(pattern="CONI-0", replacement="CONI-", x=station),
-         station = gsub(pattern="-00", replacement="-", x=station),
-         station = gsub(pattern="-0", replacement="-", x=station)) %>% 
-  inner_join(cov) %>% 
-  unique() %>% 
-  mutate(FireTime=year-FireHistory,
-         FireTime=ifelse(FireTime < 0, NA, FireTime),
-         FireTime=ifelse(is.na(FireTime), 200, FireTime),
-         FireSeverity = ifelse(year > 2017, FireSeverity, NA)) %>% 
-  dplyr::filter(!is.na(pine.300))
-
-write.csv(dat.cov, "SurveyDataWithCovs.csv", row.names = FALSE)
-
-dat.gbm <- dat.cov %>% 
+dat.gbm <- dat %>% 
   dplyr::select(boom, p.boom, call, p.call, Elevation, cover.300, develop.300, grass.300, trails.300, pine.300, sand.300, water.300, wetland.300) %>% 
   data.frame()
 
 #3. Model boom----
-
+set.seed(1234)
 boom.gbm <- dismo::gbm.step(data=dat.gbm, 
                      gbm.x=5:13,
                      gbm.y=1,
                      offset=dat.gbm$p.boom,
                      family="bernoulli",
                      tree.complexity = 3,
-                     learning.rate = 0.001,
+                     learning.rate = 0.005,
                      bag.fraction = 0.75,
                      max.trees=10000) 
 
 boom.int <- gbm.interactions(boom.gbm)
 
 #4. Model call----
+set.seed(1234)
 call.gbm <- dismo::gbm.step(data=dat.gbm, 
                             gbm.x=5:13,
                             gbm.y=3,
@@ -79,8 +47,16 @@ call.gbm <- dismo::gbm.step(data=dat.gbm,
 call.int <- gbm.interactions(call.gbm)
 
 #5. Look at results----
-summary(boom.gbm)
-summary(call.gbm)
+summary <- data.frame(summary(boom.gbm)) %>% 
+  mutate(response="boom") %>% 
+  rbind(data.frame(summary(call.gbm)) %>% 
+          mutate(response="call"))
 
-boom.int$rank.list
-call.int$rank.list
+write.csv(summary, "BRTCovariateResults.csv", row.names = FALSE)
+
+int <- data.frame(boom.int$rank.list) %>% 
+  mutate(response="boom") %>% 
+  rbind(data.frame(call.int$rank.list) %>% 
+          mutate(response="call"))
+
+write.csv(summary, "BRTInteractionResults.csv", row.names = FALSE)
