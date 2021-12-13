1409bbbab8024abdc9a7d247fe075a40885f5b33
ecknight <ellyknight@gmail.com>
2021-12-12 22:32:07 -0700

    Fix wrangling, tweak survival models

--- 06_offsets.R
+++ 06_offsets.R
@@ -9,6 +9,7 @@ library(tidyverse)
 library(readr)
 library(fs)
 library(lubridate)
+library(suncalc)
 
 options(scipen=99999)
 
@@ -17,7 +18,16 @@ options(scipen=99999)
 #1. Read in recognizer data----
 df.raw <- read.csv("Data/ValidatedRecognizerResults.csv") %>% 
   mutate(timeofdetection=as.numeric(str_sub(TimeOffset, 5, 5))*60+as.numeric(str_sub(TimeOffset, 7, 8))) %>%
-  mutate(DateTime = as_datetime(DateTime))
+  rename(path = Filename) %>% 
+  mutate(DateTime = as_datetime(DateTime),
+         path = gsub("\\", "/", path, fixed=TRUE),
+         path = gsub("/Volumes/", "", path, fixed=TRUE)) %>% 
+  separate(path, 
+           into = c("Drive", "Park", "Batch", "Filename"),
+           sep = "/",
+           remove = FALSE) %>% 
+  mutate(Date = as.character(Date)) %>% 
+  dplyr::select(Filename, Station, Date, Time, DateTime, TimeOffset, detection, timeofdetection) 
 
 #2. Add files with no detections----
 rec <- read.csv("FinalFileList.csv") %>% 
@@ -26,7 +36,7 @@ rec <- read.csv("FinalFileList.csv") %>%
            sep = "/",
            remove = FALSE) %>% 
   dplyr::filter(!is.na(Filename),
-                !Filename %in% df.raw$Station) %>% 
+                !Filename %in% df.raw$Filename) %>% 
   separate(Filename, 
            into = c("Station", "Date", "Time", "Filetype"),
            sep = "[^A-Za-z0-9 \\- :]",
@@ -35,33 +45,54 @@ rec <- read.csv("FinalFileList.csv") %>%
          TimeOffset=NA,
          timeofdetection=NA,
          Date = ymd(Date),
-         DateTime = as_datetime(paste0(Date, Time)), .after = Time) %>% 
+         Time = as.character(Time),
+         DateTime = ymd_hms(paste0(Date, Time))) %>% 
   dplyr::select(Filename, Station, Date, Time, DateTime, TimeOffset, detection, timeofdetection)
 
 df <- rbind(df.raw, rec)
 
-#3. Add new fields-----
+tz(df$DateTime) <- "Canada/Mountain"
 
-#3. ID locations with detections----
-site.boom <- df %>% 
+hist(hour(df$DateTime))
+
+#3. Add time since sunset-----
+#Use mean location, doesn't really matter
+locs <- read.csv("ExistingSamplingLocations.csv") %>% 
+  dplyr::select(X, Y) %>% 
+  summarize(X=mean(X),
+            Y=mean(Y))
+
+df.locs <- df %>% 
+  mutate(lon=locs$X,
+         lat=locs$Y) %>% 
+  mutate(date=ymd(str_sub(as.character(DateTime), 1, 10)))
+
+df.sun <- getSunlightTimes(data=df.locs, tz="Canada/Mountain", keep="sunset") %>% 
+  cbind(df) %>% 
+  mutate(tsss = as.numeric(difftime(DateTime, sunset, units="hours")),
+         tsss = ifelse(tsss < -12, tsss+24, tsss)) 
+
+ggplot(df.sun) +
+  geom_histogram(aes(x=tsss))
+
+  
+#4. ID locations with detections----
+site.boom <- df.sun %>% 
   filter(detection=="B") %>% 
   dplyr::select(Station) %>% 
   unique()
 
-df.boom <- df %>% 
-  filter(Station %in% site.boom$Station) %>% 
-  mutate(detection = ifelse(detection=="B", 1, 0))
-
-site.call <- df %>% 
+site.call <- df.sun %>% 
   filter(detection %in% c("1", "B")) %>% 
   dplyr::select(Station) %>% 
   unique()
 
-#4. Subset to time to first detection and treat nondetections as censored events----
+#5. Subset to time to first detection and treat nondetections as censored events----
 #also make 0 seconds 1 seconds
-df.boom.1st <- df %>% 
+df.boom.1st <- df.sun %>% 
   filter(Station %in% site.boom$Station) %>% 
-  mutate(detection = ifelse(detection=="B", 1, 0)) %>% 
+  mutate(detection = ifelse(detection=="B", 1, 0),
+         timeofdetection = ifelse(detection==1, timeofdetection, 600)) %>% 
   arrange(Filename, timeofdetection) %>% 
   group_by(Filename) %>% 
   mutate(order=row_number()) %>% 
@@ -72,7 +103,7 @@ df.boom.1st <- df %>%
                                      timeofdetection==0 ~ 1,
                                      !is.na(timeofdetection) ~ timeofdetection))
 
-df.call.1st <- df %>% 
+df.call.1st <- df.sun %>% 
   filter(Station %in% site.call$Station) %>% 
   mutate(detection = as.numeric(ifelse(detection=="B", 1, detection)))  %>% 
   arrange(Filename, timeofdetection) %>% 
@@ -85,48 +116,61 @@ df.call.1st <- df %>%
                                      timeofdetection==0 ~ 1,
                                      !is.na(timeofdetection) ~ timeofdetection))
 
-#5. Add additional fields----
+#6. Add additional fields----
 df.boom <- df.boom.1st %>% 
   mutate(doy = yday(DateTime),
          jday = doy/365,
          jday2 = jday^2,
-         start = hour(DateTime) + minute(DateTime)/60,
-         tod = start/24,
-         sin = sin(tod*2*pi),
-         cos = cos(tod*2*pi),
+         ts = (tsss +2)/8,
+         ts2 = ts^2,
+         sin = sin(ts*2*pi),
+         cos = cos(ts*2*pi),
          sv = Surv(time/60, detection))
+
+ggplot(df.boom) +
+  geom_point(aes(x=cos, y=sin))
+
+ggplot(df.boom) +
+  geom_point(aes(x=tsss, y=cos))
 
 df.call <- df.call.1st %>% 
   mutate(doy = yday(DateTime),
          jday = doy/365,
          jday2 = jday^2,
          start = hour(DateTime) + minute(DateTime)/60,
-         tod = start/24,
-         sin = sin(tod*2*pi),
-         cos = cos(tod*2*pi),
+         ts = (tsss +2)/8,
+         ts2 = ts^2,
+         sin = sin(ts*2*pi),
+         cos = cos(ts*2*pi),
          sv = Surv(time/60, detection))
 
-#6. Check for file duplicates----
+#7. Check for file duplicates----
 stopifnot(!any(duplicated(df.boom$Filename)))
 stopifnot(!any(duplicated(df.call$Filename)))
 
-#7. Mean of survival time in minutes----
+#8. Mean of survival time in minutes----
 mean(df.boom$sv)
 hist(df.boom$time/60)
 mean(df.call$sv)
 hist(df.call$time/60)
 
-#8. Fit a series of models----
+#9. Fit a series of models----
 mods.boom <- list(
   m0 = survreg(sv ~ 1, df.boom, dist="exponential"),
   m1 = survreg(sv ~ jday, df.boom, dist="exponential"),
   m2 = survreg(sv ~ jday + jday2, df.boom, dist="exponential"),
-  m3 = survreg(sv ~ cos, df.boom, dist="exponential"),
-  m4 = survreg(sv ~ cos + sin, df.boom, dist="exponential"),
-  m5 = survreg(sv ~ jday + cos, df.boom, dist="exponential"),
-  m6 = survreg(sv ~ jday + cos + sin, df.boom, dist="exponential"),
-  m7 = survreg(sv ~ jday + jday2 + cos, df.boom, dist="exponential"),
-  m8 = survreg(sv ~ jday + jday2 + cos + sin, df.boom, dist="exponential")
+  m3 = survreg(sv ~ sin, df.boom, dist="exponential"),
+  m4 = survreg(sv ~ cos, df.boom, dist="exponential"),
+  m5 = survreg(sv ~ sin + cos, df.boom, dist="exponential"),
+  m6 = survreg(sv ~ sin*cos, df.boom, dist="exponential"),
+  m7 = survreg(sv ~ jday + sin, df.boom, dist="exponential"),
+  m8 = survreg(sv ~ jday + cos, df.boom, dist="exponential"),
+  m9 = survreg(sv ~ jday + sin + cos, df.boom, dist="exponential"),
+  m10 = survreg(sv ~ jday + sin*cos, df.boom, dist="exponential"),
+  m11 = survreg(sv ~ jday + jday2 + sin, df.boom, dist="exponential"),
+  m12 = survreg(sv ~ jday + jday2 + cos, df.boom, dist="exponential"),
+  m13 = survreg(sv ~ jday + jday2 + sin + cos, df.boom, dist="exponential"),
+  m14 = survreg(sv ~ jday + jday2 + sin*cos, df.boom, dist="exponential")
 )
 
 
@@ -134,15 +178,21 @@ mods.call <- list(
   m0 = survreg(sv ~ 1, df.call, dist="exponential"),
   m1 = survreg(sv ~ jday, df.call, dist="exponential"),
   m2 = survreg(sv ~ jday + jday2, df.call, dist="exponential"),
-  m3 = survreg(sv ~ cos, df.call, dist="exponential"),
-  m4 = survreg(sv ~ cos + sin, df.call, dist="exponential"),
-  m5 = survreg(sv ~ jday + cos, df.call, dist="exponential"),
-  m6 = survreg(sv ~ jday + cos + sin, df.call, dist="exponential"),
-  m7 = survreg(sv ~ jday + jday2 + cos, df.call, dist="exponential"),
-  m8 = survreg(sv ~ jday + jday2 + cos + sin, df.call, dist="exponential")
+  m3 = survreg(sv ~ sin, df.call, dist="exponential"),
+  m4 = survreg(sv ~ cos, df.call, dist="exponential"),
+  m5 = survreg(sv ~ sin + cos, df.call, dist="exponential"),
+  m6 = survreg(sv ~ sin*cos, df.call, dist="exponential"),
+  m7 = survreg(sv ~ jday + sin, df.call, dist="exponential"),
+  m8 = survreg(sv ~ jday + cos, df.call, dist="exponential"),
+  m9 = survreg(sv ~ jday + sin + cos, df.call, dist="exponential"),
+  m10 = survreg(sv ~ jday + sin*cos, df.call, dist="exponential"),
+  m11 = survreg(sv ~ jday + jday2 + sin, df.call, dist="exponential"),
+  m12 = survreg(sv ~ jday + jday2 + cos, df.call, dist="exponential"),
+  m13 = survreg(sv ~ jday + jday2 + sin + cos, df.call, dist="exponential"),
+  m14 = survreg(sv ~ jday + jday2 + sin*cos, df.call, dist="exponential")
 )
 
-#9. Compare with AIC----
+#10. Compare with AIC----
 aic.boom <- data.frame(df=sapply(mods.boom, function(z) length(coef(z))),
                   AIC=sapply(mods.boom, AIC),
                   loglik = sapply(mods.boom, function(z) logLik(z))) %>%
@@ -166,9 +216,7 @@ aic.call <- data.frame(df=sapply(mods.call, function(z) length(coef(z))),
   dplyr::select(df, loglik, AICc, delta, weight)
 aic.call
 
-#Seems like cos + sin fits best for both
-
-#10. Summary of predictions----
+#11. Summary of predictions----
 #Boom
 mb.boom <- mods.boom[[which.min(aic.boom$AICc)]]
 summary(mb.boom)
@@ -189,78 +237,58 @@ summary(1/predict(mb.call))
 #' Probability of at least 1 event per 10 time units (10 mins)
 summary(1-exp(-(1/predict(mb.call))*10))
 
-#11. Visualize predictions----
-
+#12. Make predictions for figure----
 #Boom
-vjd.boom <- seq(min(df.boom$jday), max(df.boom$jday), len=51*10)
-vtd.boom <- seq(0, 23/24, len=24*10)
-pr.boom <- expand.grid(jday=vjd.boom, tod=vtd.boom)
-pr.boom$jday2 <- pr.boom$jday^2
-pr.boom$cos <- cos(pr.boom$tod * 2 * pi)
-pr.boom$sin <- sin(pr.boom$tod * 2 * pi)
-#' make predictions for the grid
-fit.boom <- 1/predict(mb.boom, newdata=pr.boom)
-summary(fit.boom)
-summary(1-exp(-fit.boom*10))
-#' we plot contours for P(detection in 10 min)
-z.boom <- matrix(1-exp(-fit.boom*10), length(vjd.boom), length(vtd.boom))
-plot(tod*24 ~ jitter(doy), df.boom, pch=19, cex=0.6, col="#80808020",
-     main="Probability of availability (10 min)",
-     xlab="Ordinal day", ylab="Hour")
-points(tod*24 ~ doy, df.boom[df.boom$detection > 0,], pch=19, cex=0.6,
-       col="#FF0040")
-l <- c(0.001, 0.01, 0.1, 0.2, 0.4)
-for (i in seq_along(l)) {
-  col <- colorRampPalette(c("blue", "red"))( length(l) )[i]
-  contour(vjd.boom*365, vtd.boom*24, z.boom, add=TRUE, col=col, levels=l[i],
-          labcex=0.8, lwd=1)
-}
+pr.boom <- data.frame(expand.grid(jday = seq(round(min(df.boom$jday), 3), round(max(df.boom$jday), 3), 0.001),
+                      ts = seq(round(min(df.boom$ts), 2), round(max(df.boom$ts), 2), 0.01))) %>% 
+  mutate(jday2 = jday^2,
+         ts2 = ts^2,
+         sin = sin(ts*2*pi),
+         cos = cos(ts*2*pi))
+pr.boom$p.boom <- 1/predict(mb.boom, newdata=pr.boom)
 
 #Call
-vjd.call <- seq(min(df.call$jday), max(df.call$jday), len=51*10)
-vtd.call <- seq(0, 23/24, len=24*10)
-pr.call <- expand.grid(jday=vjd.call, tod=vtd.call)
-pr.call$jday2 <- pr.call$jday^2
-pr.call$cos <- cos(pr.call$tod * 2 * pi)
-pr.call$sin <- sin(pr.call$tod * 2 * pi)
-#' make predictions for the grid
-fit.call <- 1/predict(mb.call, newdata=pr.call)
-summary(fit.call)
-summary(1-exp(-fit.call*10))
-#' we plot contours for P(detection in 10 min)
-z.call <- matrix(1-exp(-fit.call*10), length(vjd.call), length(vtd.call))
-plot(tod*24 ~ jitter(doy), df.call, pch=19, cex=0.6, col="#80808020",
-     main="Probability of availability (10 min)",
-     xlab="Ordinal day", ylab="Hour")
-points(tod*24 ~ doy, df.call[df.call$detection > 0,], pch=19, cex=0.6,
-       col="#FF0040")
-l <- c(0.001, 0.01, 0.1, 0.2, 0.4)
-for (i in seq_along(l)) {
-  col <- colorRampPalette(c("blue", "red"))( length(l) )[i]
-  contour(vjd.call*365, vtd.call*24, z.call, add=TRUE, col=col, levels=l[i],
-          labcex=0.8, lwd=1)
-}
+pr.call <- data.frame(expand.grid(jday = seq(round(min(df.call$jday), 3), round(max(df.call$jday), 3), 0.001),
+                                  ts = seq(round(min(df.call$ts), 2), round(max(df.call$ts), 2), 0.01))) %>% 
+  mutate(jday2 = jday^2,
+         ts2 = ts^2,
+         sin = sin(ts*2*pi),
+         cos = cos(ts*2*pi))
+pr.call$p.call <- 1/predict(mb.call, newdata=pr.call)
 
-#12. Calculate offsets for recognizer data----
-df.aru <- df %>% 
+
+preds <- full_join(pr.boom, pr.call) %>% 
+  mutate(tsss = ts*8-2,
+         doy = jday*365,
+         sin = sin(ts*2*pi),
+         cos = cos(ts*2*pi))
+
+write.csv(preds, "OffsetPredictionsForFigure.csv", row.names = FALSE)
+
+write.csv(df.boom, "OffsetDataForFigure_Boom.csv", row.names = FALSE)
+write.csv(df.call, "OffsetDataForFigure_Call.csv", row.names = FALSE)
+
+#13. Calculate offsets for recognizer data----
+df.aru <- df.sun %>% 
   mutate(detection = ifelse(detection=="B", 2, as.numeric(detection))) %>% 
-  group_by(Filename, Station, DateTime) %>% 
+  group_by(Filename, Station, DateTime, tsss) %>% 
   summarize(detection=max(detection)) %>% 
   ungroup() %>% 
   mutate(doy = yday(DateTime),
          jday = doy/365,
          jday2 = jday^2,
          start = hour(DateTime) + minute(DateTime)/60,
-         tod = start/24,
-         sin = sin(tod*2*pi),
-         cos = cos(tod*2*pi),
+         ts = (tsss +2)/8,
+         ts2 = ts^2,
+         sin = ts*2*pi,
+         cos = ts*2*pi,
          survey="ARU") %>% 
   rename(station=Station)
 df.aru$p.boom <- 1-exp(-(1/predict(mb.boom, newdata = df.aru))*10)
 df.aru$p.call <- 1-exp(-(1/predict(mb.call, newdata = df.aru))*10)
 
-#13. Calculate offsets for human data----
-df.hum <- read.csv("Data/HumanData.csv") %>% 
+#14. Calculate offsets for human data----
+df.hum.raw <- read.csv("Data/HumanData.csv") %>% 
   dplyr::filter(!is.na(Start.time)) %>% 
   mutate(Start.time=ymd_hm(paste0(Date, Start.time)),
          End.time=ymd_hm(paste0(Date, End.time)),
@@ -271,22 +299,39 @@ df.hum <- read.csv("Data/HumanData.csv") %>%
   ungroup() %>% 
   mutate(Interval=Duration/Stops,
          DateTime = Start.time + Interval*ID,
-         doy = yday(DateTime),
+         lon=locs$X,
+         lat=locs$Y,
+         date=ymd(str_sub(as.character(DateTime), 1, 10)))
+
+tz(df.hum.raw$DateTime) <- "Canada/Mountain"
+
+df.hum.sun <- getSunlightTimes(data=df.hum.raw, tz="Canada/Mountain", keep="sunset") %>% 
+  cbind(df.hum.raw %>% 
+          dplyr::select(-date, -lat, -lon)) %>% 
+  mutate(tsss = as.numeric(difftime(DateTime, sunset, units="hours")),
+         tsss = ifelse(tsss < -12, tsss+24, tsss)) 
+
+ggplot(df.hum.sun) +
+  geom_histogram(aes(x=tsss))
+
+df.hum <- df.hum.sun %>% 
+  mutate(doy = yday(DateTime),
          jday = doy/365,
          jday2 = jday^2,
          start = hour(DateTime) + minute(DateTime)/60,
-         tod = start/24,
-         sin = sin(tod*2*pi),
-         cos = cos(tod*2*pi),
+         ts = (tsss +2)/8,
+         ts2 = ts^2,
+         sin = ts*2*pi,
+         cos = ts*2*pi,
          survey="human") %>% 
   rename(station=Site, detection=Presence)
 df.hum$p.boom <- 1-exp(-(1/predict(mb.boom, newdata = df.hum))*6)
 df.hum$p.call <- 1-exp(-(1/predict(mb.call, newdata = df.hum))*6)
+         
 
-
-#14. Correct for differences in recorder & recognizer EDR----
+#15. Correct for differences in recorder & recognizer EDR----
 #From Yip et al. Fig 2A SM2 @ 4000 Hz
-SM2 <- 0.7 
+SM2 <- 0.789 
 
 #Recall at score 60 from known distance clips
 rec <-  0.5626959
@@ -294,12 +339,17 @@ rec <-  0.5626959
 df.hum$p.boom <- df.hum$p.boom*(1/SM2)*(1/rec)
 df.hum$p.call <- df.hum$p.call*(1/SM2)*(1/rec)
 
+summary(df.hum$p.boom)
+summary(df.hum$p.call)
+
 #15. Put together----
 df.pred <- rbind(df.aru %>% 
-                   dplyr::select(survey, station, DateTime, detection, p.boom, p.call),
+                   dplyr::select(survey, station, DateTime, tsss, detection, p.boom, p.call),
                  df.hum  %>% 
-                   dplyr::select(survey, station, DateTime, detection, p.boom, p.call)) %>% 
+                   dplyr::select(survey, station, DateTime, tsss, detection, p.boom, p.call)) %>% 
   mutate(year=year(DateTime),
-         fire=ifelse(year <=2017, "before", "after"))
+         doy=yday(DateTime),
+         fire=ifelse(year <=2017, "before", "after")) %>% 
+  dplyr::filter(doy < 225)
 
 write.csv(df.pred, "SurveyDataWithOffsets.csv", row.names=FALSE)
