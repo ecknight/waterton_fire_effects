3da194f37a8c507a11c5115a3ed57de2da2ce961
ecknight <ellyknight@gmail.com>
2022-02-20 12:12:02 -0700

    Add extraterritorial offsets

--- 06_offsets.R
+++ 06_offsets.R
@@ -6,7 +6,7 @@ library(mefa4)
 library(maptools)
 library(survival)
 library(tidyverse)
-library(tidylog)
+#library(tidylog)
 library(readr)
 library(fs)
 library(lubridate)
@@ -93,6 +93,10 @@ site.call <- df.sun %>%
   dplyr::select(Station) %>% 
   unique()
 
+site.extra <- site.call %>% 
+  dplyr::filter(!Station %in% site.boom$Station)
+
+
 #5. Subset to time to first detection and treat nondetections as censored events----
 #also make 0 seconds 1 seconds
 df.boom.1st <- df.sun %>% 
@@ -121,6 +125,19 @@ df.call.1st <- df.sun %>%
   mutate(time = case_when(is.na(timeofdetection) ~ 600,
                                      timeofdetection==0 ~ 1,
                                      !is.na(timeofdetection) ~ timeofdetection))
+
+df.extra.1st <- df.sun %>% 
+  filter(Station %in% site.extra$Station) %>% 
+  mutate(detection = as.numeric(ifelse(detection=="B", 1, detection)))  %>% 
+  arrange(Filename, timeofdetection) %>% 
+  group_by(Filename) %>% 
+  mutate(order=row_number()) %>% 
+  ungroup() %>% 
+  dplyr::filter(order==1) %>% 
+  dplyr::select(-order) %>% 
+  mutate(time = case_when(is.na(timeofdetection) ~ 600,
+                          timeofdetection==0 ~ 1,
+                          !is.na(timeofdetection) ~ timeofdetection))
 
 #6. Add additional fields----
 df.boom <- df.boom.1st %>% 
@@ -154,15 +171,29 @@ df.call <- df.call.1st %>%
          cos = cos(ts*2*pi),
          sv = Surv(time/60, detection))
 
+df.extra <- df.extra.1st %>% 
+  mutate(doy = yday(DateTime),
+         jday = doy/365,
+         jday2 = jday^2,
+         start = hour(DateTime) + minute(DateTime)/60,
+         tod = start/24,
+         ts = (tsss +2)/8,
+         sin = sin(ts*2*pi),
+         cos = cos(ts*2*pi),
+         sv = Surv(time/60, detection))
+
 #7. Check for file duplicates----
 stopifnot(!any(duplicated(df.boom$Filename)))
 stopifnot(!any(duplicated(df.call$Filename)))
+stopifnot(!any(duplicated(df.extra$Filename)))
 
 #8. Mean of survival time in minutes----
 mean(df.boom$sv)
 hist(df.boom$time/60)
 mean(df.call$sv)
 hist(df.call$time/60)
+mean(df.extra$sv)
+hist(df.extra$time/60)
 
 #9. Fit a series of models----
 mods.boom <- list(
@@ -193,6 +224,19 @@ mods.call <- list(
   m10 = survreg(sv ~ jday + jday2 + cos, df.call, dist="exponential"),
   m11 = survreg(sv ~ jday + jday2 + sin + cos, df.call, dist="exponential"))
 
+mods.extra <- list(
+  m0 = survreg(sv ~ 1, df.extra, dist="exponential"),
+  m1 = survreg(sv ~ jday, df.extra, dist="exponential"),
+  m2 = survreg(sv ~ jday + jday2, df.extra, dist="exponential"),
+  m3 = survreg(sv ~ sin, df.extra, dist="exponential"),
+  m4 = survreg(sv ~ cos, df.extra, dist="exponential"),
+  m5 = survreg(sv ~ sin + cos, df.extra, dist="exponential"),
+  m6 = survreg(sv ~ jday + sin, df.extra, dist="exponential"),
+  m7 = survreg(sv ~ jday + cos, df.extra, dist="exponential"),
+  m8 = survreg(sv ~ jday + sin + cos, df.extra, dist="exponential"),
+  m9 = survreg(sv ~ jday + jday2 + sin, df.extra, dist="exponential"),
+  m10 = survreg(sv ~ jday + jday2 + cos, df.extra, dist="exponential"),
+  m11 = survreg(sv ~ jday + jday2 + sin + cos, df.extra, dist="exponential"))
 
 #10. Compare with AIC----
 aic.boom <- data.frame(df=sapply(mods.boom, function(z) length(coef(z))),
@@ -218,6 +262,17 @@ aic.call <- data.frame(df=sapply(mods.call, function(z) length(coef(z))),
   dplyr::select(df, loglik, AICc, delta, weight)
 aic.call
 
+aic.extra <- data.frame(df=sapply(mods.extra, function(z) length(coef(z))),
+                       AIC=sapply(mods.extra, AIC),
+                       loglik = sapply(mods.extra, function(z) logLik(z))) %>%
+  mutate(AICc = AIC + (2*df^2+2*df) / (length(df.extra)-df-1),
+         delta = AICc - min(AICc),
+         rellike = exp(-.5*delta),
+         weight = rellike/sum(rellike)) %>%
+  mutate_at(c("loglik", "AICc", "delta", "weight"), ~round(., 2)) %>%
+  dplyr::select(df, loglik, AICc, delta, weight)
+aic.extra
+
 #11. Summary of predictions----
 #Boom
 mb.boom <- mods.boom[[which.min(aic.boom$AICc)]]
@@ -239,6 +294,16 @@ summary(1/predict(mb.call))
 #' Probability of at least 1 event per 10 time units (10 mins)
 summary(1-exp(-(1/predict(mb.call))*10))
 
+#Extra
+mb.extra <- mods.extra[[which.min(aic.extra$AICc)]]
+summary(mb.extra)
+#' Survival times
+summary(predict(mb.extra))
+#' Event rate per unit (1 min) time
+summary(1/predict(mb.extra))
+#' Probability of at least 1 event per 10 time units (10 mins)
+summary(1-exp(-(1/predict(mb.extra))*10))
+
 #12. Make predictions for figure----
 #Boom
 pr.boom <- data.frame(expand.grid(jday = seq(round(min(df.boom$jday), 3), round(max(df.boom$jday), 3), 0.001),
@@ -258,8 +323,18 @@ pr.call <- data.frame(expand.grid(jday = seq(round(min(df.call$jday), 3), round(
          cos = cos(ts*2*pi))
 pr.call$p.call <- 1/predict(mb.call, newdata=pr.call)
 
+#Extra
+pr.extra <- data.frame(expand.grid(jday = seq(round(min(df.extra$jday), 3), round(max(df.extra$jday), 3), 0.001),
+                                  ts = seq(round(min(df.extra$ts), 2), round(max(df.extra$ts), 2), 0.01))) %>% 
+  mutate(jday2 = jday^2,
+         ts2 = ts^2,
+         sin = sin(ts*2*pi),
+         cos = cos(ts*2*pi))
+pr.extra$p.extra <- 1/predict(mb.extra, newdata=pr.extra)
+
 
 preds <- full_join(pr.boom, pr.call) %>% 
+  full_join(pr.extra) %>% 
   mutate(tsss = ts*8-2,
          doy = jday*365,
          sin = sin(ts*2*pi),
@@ -268,6 +343,7 @@ preds <- full_join(pr.boom, pr.call) %>%
 write.csv(preds, "OffsetPredictionsForFigure.csv", row.names = FALSE)
 write.csv(df.boom, "OffsetDataForFigure_Boom.csv", row.names = FALSE)
 write.csv(df.call, "OffsetDataForFigure_Call.csv", row.names = FALSE)
+write.csv(df.extra, "OffsetDataForFigure_Extra.csv", row.names = FALSE)
 
 #13. Calculate offsets for recognizer data----
 df.aru <- df.sun %>% 
@@ -288,6 +364,7 @@ df.aru <- df.sun %>%
   rename(station=Station)
 df.aru$p.boom <- 1-exp(-(1/predict(mb.boom, newdata = df.aru))*10)
 df.aru$p.call <- 1-exp(-(1/predict(mb.call, newdata = df.aru))*10)
+df.aru$p.extra <- 1-exp(-(1/predict(mb.extra, newdata = df.aru))*10)
 
 #14. Calculate offsets for human data----
 df.hum.raw <- read.csv("Data/HumanData.csv") %>% 
@@ -329,8 +406,8 @@ df.hum <- df.hum.sun %>%
   rename(station=Site, detection=Presence)
 df.hum$p.boom <- NA
 df.hum$p.call <- 1-exp(-(1/predict(mb.call, newdata = df.hum))*6)
+df.hum$p.extra <- NA
          
-
 #15. Correct for differences in recorder & recognizer EDR----
 #From Yip et al. Fig 2A SM2 @ 4000 Hz
 SM2 <- 0.789 
@@ -345,15 +422,16 @@ summary(df.hum$p.call)
 
 #15. Put together----
 df.pred <- rbind(df.aru %>% 
-                   dplyr::select(survey, station, DateTime, tsss, detection, p.boom, p.call),
+                   dplyr::select(survey, station, DateTime, tsss, detection, p.boom, p.call, p.extra),
                  df.hum  %>% 
-                   dplyr::select(survey, station, DateTime, tsss, detection, p.boom, p.call)) %>%
+                   dplyr::select(survey, station, DateTime, tsss, detection, p.boom, p.call, p.extra)) %>%
   mutate(year=year(DateTime),
          doy=yday(DateTime),
          fire=ifelse(year <=2017, "before", "after")) %>% 
   dplyr::filter(doy < 225) %>% 
   data.frame() %>% 
-  mutate(p.call = p.call/(max(p.call)))
+  mutate(p.call = p.call/(max(p.call))) %>% 
+  mutate(detection = ifelse(station %in% site.extra$Station & detection !=0, 3, detection))
 
 write.csv(df.pred, "SurveyDataWithOffsets.csv", row.names=FALSE)
 
