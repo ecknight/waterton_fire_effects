2e8d1b53b0d570ad31eacb051c1fa82af7b4fe19
ecknight <ellyknight@gmail.com>
2022-02-20 12:12:56 -0700

    Add separate call abundance estimation

--- 09_Abundance.R
+++ 09_Abundance.R
@@ -10,23 +10,42 @@ options(scipen=99999)
 source("src/zi.fit.R")
 
 #1. Wrangling----
-dat.aru <- read.table("Data/IndividualID_validated.txt")
-colnames(dat.aru) <- c("filename", "time", "duration", "level", "quality", "score", "detection", "individual")
+dat.aru.b <- read.table("Data/IndividualID_boom_validated.txt")
+dat.aru.c <- read.table("Data/IndividualID_call_validated.txt")
+colnames(dat.aru.b) <- c("filename", "time", "duration", "level", "quality", "score", "detection", "individual")
+colnames(dat.aru.c) <- c("filename", "time", "duration", "level", "quality", "score", "detection", "individual")
 
-abu.aru <- dat.aru %>% 
-  dplyr::filter(individual!="N") %>% 
+abu.aru.b <- dat.aru.b %>% 
+  dplyr::filter(individual!="N",
+                detection=="B") %>% 
   mutate(individualUse = as.numeric(str_sub(individual, 1, 1))) %>% 
-  group_by(filename, detection) %>% 
+  group_by(filename) %>% 
   summarize(abundance = max(individualUse)) %>% 
   ungroup() %>% 
   mutate(survey="ARU") %>% 
   separate(filename, into=c("blank", "volumes", "hd", "project", "folder", "file"), sep="/", remove=FALSE) %>% 
   separate(file, into=c("station", "datename", "timename"), sep="_", remove=FALSE) %>% 
   mutate(date = ymd(datename),
-         year = year(date),
-         detection = case_when(detection=="1" ~ "call", 
-                               detection=="B" ~ "boom")) %>% 
-  dplyr::select(survey, station, year, date, detection, abundance)
+         year = year(date)) %>% 
+  dplyr::select(survey, station, year, date, abundance)
+
+abu.aru.c <- dat.aru.c %>% 
+  dplyr::filter(individual!="N",
+                detection=="1") %>% 
+  mutate(individualUse = as.numeric(str_sub(individual, 1, 1))) %>% 
+  group_by(filename) %>% 
+  summarize(abundance = max(individualUse)) %>% 
+  ungroup() %>% 
+  mutate(survey="ARU") %>% 
+  separate(filename, into=c("blank", "volumes", "hd", "project", "folder", "file"), sep="/", remove=FALSE) %>% 
+  separate(file, into=c("station", "datename", "timename"), sep="_", remove=FALSE) %>% 
+  mutate(date = ymd(datename),
+         year = year(date)) %>% 
+  dplyr::select(survey, station, year, date, abundance)
+
+abu.aru.x <- abu.aru.c %>% 
+  anti_join(abu.aru.b %>% 
+              dplyr::select(survey, station, year))
 
 dat.hum <- read.csv("Data/HumanData.csv")
 
@@ -36,19 +55,20 @@ abu.hum <- dat.hum %>%
   mutate(detection = "call") %>% 
   dplyr::select(survey, station, year, date, detection, abundance)
 
-abu <- abu.aru %>% 
+abu.boom <- abu.aru.b %>% 
   mutate(fire = ifelse(year <= 2017, "before", "after")) %>% 
   mutate(firesurvey = paste0(fire,"-", survey))
 
-#abu <- rbind(abu.aru, abu.hum) %>% 
-#  mutate(fire = ifelse(year <= 2017, "before", "after")) %>% 
-#  mutate(firesurvey = paste0(fire,"-", survey))
+abu.call <- abu.aru.c %>% 
+  mutate(fire = ifelse(year <= 2017, "before", "after")) %>% 
+  mutate(firesurvey = paste0(fire,"-", survey))
 
-abu.boom <- abu %>% 
-  dplyr::filter(detection=="boom")
-
-abu.call <- abu %>% 
-  dplyr::filter(detection=="call")
+abu.extra <- abu.aru.c %>% 
+  anti_join(abu.aru.b %>% 
+              dplyr::select(survey, station, year)) %>% 
+  mutate(fire = ifelse(year <= 2017, "before", "after")) %>% 
+  mutate(firesurvey = paste0(fire,"-", survey))
+  
 
 #2. Visualize----
 ggplot(abu.boom) +
@@ -56,6 +76,10 @@ ggplot(abu.boom) +
   facet_grid(fire ~ survey)
 
 ggplot(abu.call) +
+  geom_histogram(aes(x=abundance)) +
+  facet_grid(fire ~ survey)
+
+ggplot(abu.extra) +
   geom_histogram(aes(x=abundance)) +
   facet_grid(fire ~ survey)
 
@@ -103,6 +127,27 @@ ic <- ic[order(ic$AICc),] %>%
 ic
 
 cl0p.call <- cl0p
+
+#Extraterritorial
+Y1 <- abu.extra$abundance
+X0 <- data.matrix(rep(1, length(Y1)))
+Z0 <- X0
+
+cl0p <- zi.fit(Y1, X0, Z0, distr="pois", type="CL", hessian=TRUE)$CL
+cl0nb <- zi.fit(Y1, X0, Z0, distr="negbin", type="CL", hessian=TRUE)$CL
+
+ic <- AIC(cl0p, cl0nb)
+ic$loglik <- c(logLik(cl0p), logLik(cl0nb))
+ic$BIC <- AIC(cl0p,cl0nb, k=log(length(Y1)))$AIC
+ic$AICc <- ic$AIC + (2*ic$df^2+2*ic$df) / (length(Y1)-ic$df-1)
+ic <- ic[order(ic$AICc),] %>% 
+  mutate(delta = AICc - head(AICc, 1),
+         rellike = exp(-.5*delta),
+         weight = rellike/sum(rellike)) %>% 
+  dplyr::select(df, loglik, AICc, delta, weight)
+ic
+
+cl0p.extra <- cl0p
 #Poisson  with no covs for both detection types
 
 #4. Estimate
@@ -125,8 +170,18 @@ exp(-lambda.call)
 ## suitable and occupied
 1-exp(-lambda.call)
 
+#Extraterritorial
+## mean of the poisson (including 0 counts and offsets too)
+lambda.extra <- exp(cl0p.extra$coef)
+lambda.extra
+## P(N=0) based on Poisson, which is the suitable but unoccupied probability
+exp(-lambda.extra)
+## suitable and occupied
+1-exp(-lambda.extra)
+
 #5. Save out----
-lambda <- data.frame(detection=c("boom", "call"),
-                     lambda=c(lambda.boom, lambda.call))
+lambda <- data.frame(detection=c("boom", "call", "extra"),
+                     lambda=c(lambda.boom, lambda.call, lambda.extra))
+lambda
 
 write.csv(lambda, "LambdaEstimates.csv", row.names = FALSE)
