405ce847ad250b1bcd426ddc89c002b6c5bf9824
ecknight <ellyknight@gmail.com>
2022-02-28 10:10:40 -0700

    Add CI estimates

--- 11_SpatialPrediction.R
+++ 11_SpatialPrediction.R
@@ -31,19 +31,35 @@ pred <- grass.300 %>%
          elevation = round(elevation)) %>% 
   left_join(newdat) %>% 
   mutate(lambda.boom = lambda$lambda[1],
+         lambda.boom.high = lambda$quanthigh[1],
+         lambda.boom.low = lambda$quantlow[1],
          lambda.call = lambda$lambda[2],
+         lambda.call.high = lambda$quanthigh[2],
+         lambda.call.low = lambda$quantlow[2],
          densityha.boom = delta.boom*lambda.boom/edr,
+         densityha.boom.high = delta.boom.high*lambda.boom.high/edr,
+         densityha.boom.low = delta.boom.low*lambda.boom.low/edr,
          densityha.call = delta.call*lambda.call/edr,
+         densityha.call.high = delta.call.high*lambda.call.high/edr,
+         densityha.call.low = delta.call.low*lambda.call.low/edr,
          cellsize = 900,
          densitycell.boom = densityha.boom*cellsize/10000,
-         densitycell.call = densityha.call*cellsize/10000)
+         densitycell.boom.high = densityha.boom.high*cellsize/10000,
+         densitycell.boom.low = densityha.boom.low*cellsize/10000,
+         densitycell.call = densityha.call*cellsize/10000,
+         densitycell.call.high = densityha.call.high*cellsize/10000,
+         densitycell.call.low = densityha.call.low*cellsize/10000)
   
 write.csv(pred, "SpatialPredictions.csv", row.names = FALSE)
 
 #3. Population estimates----
 pop <- pred %>% 
   summarize(pop.boom = sum(densitycell.boom, na.rm=TRUE),
+            pop.boom.high = sum(densitycell.boom.high, na.rm=TRUE),
+            pop.boom.low = sum(densitycell.boom.low, na.rm=TRUE),
             pop.call = sum(densitycell.call, na.rm=TRUE),
+            pop.call.high = sum(densitycell.call.high, na.rm=TRUE),
+            pop.call.low = sum(densitycell.call.low, na.rm=TRUE),
             cells= n())
 pop
 
@@ -73,5 +89,7 @@ ggsave(plot.density,
        file = "Figs/SpatialPredictions_Facet.jpeg", width = 16, height = 7, device="jpeg")
 
 #5. Territory size----
-boom.max <- max(pred$densitycell.boom)
-territory <- 1/boom.max*0.4229*0.3041259
+#For most dense areas
+territory <- 1/max(pred$densityha.boom)
+territory.high <- 1/max(pred$densityha.boom.high)
+territory.low <- 1/max(pred$densityha.boom.low)
