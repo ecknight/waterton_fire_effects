69fa6b6535b06c5e37321fa1afc782b7b8b7e05d
ecknight <ellyknight@gmail.com>
2022-02-28 10:09:50 -0700

    rerun without height

--- 08_BRT.R
+++ 08_BRT.R
@@ -5,6 +5,7 @@
 library(tidyverse)
 library(lubridate)
 library(dismo)
+library(gridExtra)
 
 #idea: only use ARU data and summarize at station level
 
@@ -17,7 +18,7 @@ dat <- read.csv("SurveyDataWithCovs.csv") %>%
          DateTime = ymd_hms(DateTime),
          doy = yday(DateTime)) %>% 
   dplyr::filter(survey=="ARU") %>% 
-  group_by(ID, Elevation, cover.300, develop.300, grass.300, pine.300, water.300, wetland.300, evi) %>% 
+  group_by(ID, Elevation, develop.300, grass.300, pine.300, water.300, wetland.300, evi) %>% 
   summarize(boom = ifelse(sum(boom) > 0, 1, 0),
             call= ifelse(sum(call) > 0, 1, 0),
             extra = ifelse(sum(extra) > 0, 1, 0),
@@ -25,14 +26,14 @@ dat <- read.csv("SurveyDataWithCovs.csv") %>%
             p.call = mean(p.call),
             p.extra = mean(p.extra)) %>% 
   ungroup() %>% 
-  dplyr::select(boom, p.boom, call, p.call, extra, p.extra, Elevation, cover.300, develop.300, grass.300,  pine.300, water.300, wetland.300, evi) %>% 
+  dplyr::select(boom, p.boom, call, p.call, extra, p.extra, Elevation, develop.300, grass.300,  pine.300, water.300, wetland.300, evi) %>% 
   data.frame() %>% 
   dplyr::filter(Elevation > 0)
 
 #3. Model boom----
 set.seed(1234)
 boom.gbm <- dismo::gbm.step(data=dat, 
-                     gbm.x=7:14,
+                     gbm.x=7:13,
                      gbm.y=1,
                      offset=dat$p.boom,
                      family="bernoulli",
@@ -48,7 +49,7 @@ boom.int <- gbm.interactions(boom.gbm)
 #4. Model call----
 set.seed(1234)
 call.gbm <- dismo::gbm.step(data=dat, 
-                            gbm.x=7:14,
+                            gbm.x=7:13,
                             gbm.y=3,
                             offset=dat$p.call,
                             family="bernoulli",
@@ -64,7 +65,7 @@ call.int <- gbm.interactions(call.gbm)
 #5. Model extraterritorial----
 set.seed(1234)
 extra.gbm <- dismo::gbm.step(data=dat, 
-                            gbm.x=7:14,
+                            gbm.x=7:13,
                             gbm.y=5,
                             offset=dat$p.extra,
                             family="bernoulli",
@@ -87,7 +88,12 @@ summary <- data.frame(summary(boom.gbm)) %>%
 
 View(summary)
 
-write.csv(summary, "BRTCovariateResults.csv", row.names = FALSE)
+brt.t1 <- summary %>% 
+  dplyr::filter(response!="extra") %>% 
+  mutate(rel.inf = round(rel.inf, 2)) %>% 
+  pivot_wider(names_from=response, values_from=rel.inf)
+
+write.csv(brt.t1, "BRTCovariateResults.csv", row.names = FALSE)
 
 int <- data.frame(boom.int$rank.list) %>% 
   mutate(response="boom") %>% 
@@ -97,4 +103,16 @@ int <- data.frame(boom.int$rank.list) %>%
           mutate(response="extra"))
 int
 
-write.csv(int, "BRTInteractionResults.csv", row.names = FALSE)
+brt.t2 <- int %>% 
+  dplyr::filter(response!="extra") %>% 
+  dplyr::select(-var1.index, -var2.index) %>% 
+  mutate(int.size = round(int.size, 2)) %>% 
+  pivot_wider(names_from=response, values_from=int.size)
+
+write.csv(brt.t2, "BRTInteractionResults.csv", row.names = FALSE)
+
+#6. Make a figure for appendix 1---
+boom.plot <- gbm.plot(boom.gbm, n.plots=3, plot.layout = c(1,3), y.label="Marginal effect on territorial occurrence", write.title = FALSE)
+call.plot <- gbm.plot(call.gbm, n.plots=3, plot.layout = c(1,3), y.label="Marginal effect on home range occurrence", write.title = FALSE)
+
+grid.arrange(boom.plot, call.plot, nrow=2)
